c8087dfb5622ee2ff22eaa7796f4566f292f7938
Chad <40812274+MrWittman612@users.noreply.github.com>
2022-09-01 02:31:42 -0500

    changed file ext name mongoose uri string

--- utils/connectMongoDB.js
+++ utils/connectMongoDB.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const uri = 'mongodb://localhost:27017/simple-express-app';
+const uri = 'mongodb://127.0.0.1:27017/simple-express-app';
 
 module.exports = (connectMongoDB) => {
   mongoose
